8e656dfef0b4085c2b148b8b2d40e9082ea2b11d
tim-po <p.hna@mail.ru>
2017-03-13 01:31:26 +0300

    1.4
    cosmetic changes

--- bot.py
+++ bot.py
@@ -5,6 +5,24 @@ from telebot import types
 bot = telebot.TeleBot(config.token)
 
 
+@bot.inline_handler(lambda query: len(query.query) >= 0)
+def query_text(inline_query):
+    yandex_icon = "https://lh5.ggpht.com/g40UQh7fmsg0oLRNpF4b06zeGBOwerbepCXTPz-HSwTSBBZzPODjzjJWkDUrX2v6p60=w300"
+    bot_icon = "https://telegram.org/file/811140614/2/flKQKZ7xUOE.27938.gif/5574a04570218c9e11"
+    try:
+        translation = types.InlineQueryResultArticle(
+            '1', 'translation', types.InputTextMessageContent('translated_text'),
+        thumb_url = yandex_icon, thumb_width = 48, thumb_height = 48
+        )
+        suggestion = types.InlineQueryResultArticle(
+            '2', 'bot contact', types.InputTextMessageContent('for language setup https://telegram.me/TranslateInBot'),
+            thumb_url=bot_icon, thumb_width=48, thumb_height=48
+        )
+        bot.answer_inline_query(inline_query.id, [translation, suggestion])
+    except Exception as e:
+        print(e)
+
+
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.send_message(message.chat.id,  """\
@@ -17,7 +35,6 @@ Welkom to Translator bot settings, here you could set your translation language.
     bot.send_message(message.chat.id, "you can:", reply_markup=markup)
 
 
-
 @bot.message_handler(func=lambda message: message.text == 'change language')
 def open_languge_setings(message):
     markup = types.ReplyKeyboardMarkup(row_width=1)
@@ -26,6 +43,7 @@ def open_languge_setings(message):
     itembtn3 = types.KeyboardButton('Russian')
     markup.add(itembtn1, itembtn2, itembtn3)
     bot.send_message(message.chat.id, "Choose language:", reply_markup=markup)
+
 
 @bot.message_handler(func=lambda message: len(message.text) > 0)
 def change_language(message):
