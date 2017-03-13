372824f1b16bc85f4ac0407551345c3cb0860a30
tim-po <p.hna@mail.ru>
2017-03-13 08:30:01 +0300

    1.4
    Inline bot is ready.

--- bot.py
+++ bot.py
@@ -1,26 +1,14 @@
 import config
-import json
+import inline_moduel
+import language_setings
 import telebot
 from telebot import types
 bot = telebot.TeleBot(config.token)
 
 
 @bot.inline_handler(lambda query: len(query.query) >= 0)
-def query_text(inline_query):
-    yandex_icon = "https://lh5.ggpht.com/g40UQh7fmsg0oLRNpF4b06zeGBOwerbepCXTPz-HSwTSBBZzPODjzjJWkDUrX2v6p60=w300"
-    bot_icon = "https://telegram.org/file/811140614/2/flKQKZ7xUOE.27938.gif/5574a04570218c9e11"
-    try:
-        translation = types.InlineQueryResultArticle(
-            '1', 'translation', types.InputTextMessageContent('translated_text'),
-        thumb_url = yandex_icon, thumb_width = 48, thumb_height = 48
-        )
-        suggestion = types.InlineQueryResultArticle(
-            '2', 'bot contact', types.InputTextMessageContent('for language setup https://telegram.me/TranslateInBot'),
-            thumb_url=bot_icon, thumb_width=48, thumb_height=48
-        )
-        bot.answer_inline_query(inline_query.id, [translation, suggestion])
-    except Exception as e:
-        print(e)
+def inline_handle(inline_query):
+    inline_moduel.handle_inline_query(bot, inline_query)
 
 
 @bot.message_handler(commands=['help', 'start'])
@@ -29,30 +17,20 @@ def send_welcome(message):
 Welkom to Translator bot settings, here you could set your translation language.\
 """)
     markup = types.ReplyKeyboardMarkup(row_width=1)
-    itembtn1 = types.KeyboardButton('change language')
-    itembtn2 = types.KeyboardButton('v')
-    markup.add(itembtn1, itembtn2)
+    welcom_buton1 = types.KeyboardButton('change language')
+    welcom_buton2 = types.KeyboardButton('other option')
+    markup.add(welcom_buton1, welcom_buton2)
     bot.send_message(message.chat.id, "you can:", reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message: message.text == 'change language')
-def open_languge_setings(message):
-    markup = types.ReplyKeyboardMarkup(row_width=1)
-    itembtn1 = types.KeyboardButton('English')
-    itembtn2 = types.KeyboardButton('French')
-    itembtn3 = types.KeyboardButton('Russian')
-    markup.add(itembtn1, itembtn2, itembtn3)
-    bot.send_message(message.chat.id, "Choose language:", reply_markup=markup)
+def message_handler(message):
+    language_setings.open_languge_setings(message)
 
 
-@bot.message_handler(func=lambda message: len(message.text) > 0)
-def change_language(message):
-    user_data = json.dumps({"id": message.chat.id, "language": message.text}, sort_keys=True)
-    file = open('text.txt', 'a')
-    file.write(user_data + '\n')
-    markup = types.ReplyKeyboardRemove(selective=False)
-    bot.send_message(message.chat.id, "now your translation language is "+message.text, reply_markup=markup)
-
+@bot.message_handler(func=lambda message: message.text[0] == ':')
+def message_handler(message):
+    language_setings.change_language(message)
 
 
 
---
+++ language_setings.py
@@ -0,0 +1,22 @@
+import config
+import telebot
+import json
+from telebot import types
+
+def change_language(message):
+    bot = telebot.TeleBot(config.token)
+    user_data = json.dumps({"id": message.chat.id, "language": message.text}, sort_keys=True)
+    file = open('database.txt', 'a')
+    file.write(user_data + '\n')
+    markup = types.ReplyKeyboardRemove(selective=False)
+    bot.send_message(message.chat.id, "now your translation language is " + message.text, reply_markup=markup)
+
+
+def open_languge_setings(message):
+    bot = telebot.TeleBot(config.token)
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    itembtn1 = types.KeyboardButton(':English')
+    itembtn2 = types.KeyboardButton(':French')
+    itembtn3 = types.KeyboardButton(':Russian')
+    markup.add(itembtn1, itembtn2, itembtn3)
+    bot.send_message(message.chat.id, "Choose language:", reply_markup=markup)
