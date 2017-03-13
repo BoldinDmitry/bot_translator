ecd3cfb7a3e22cc84f5d07765403afe90c895e6a
tim-po <p.hna@mail.ru>
2017-03-13 23:52:54 +0300

    1.6.1

--- bot.py
+++ bot.py
@@ -14,26 +14,17 @@ def inline_handle(inline_query):
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-<<<<<<< HEAD
     bot.send_message(message.chat.id, """\
-Welkom to Translator bot settings, here you could set your translation language.\
 Welcome to Translator bot settings, here you could set your translation language.\
 """)
     markup = types.ReplyKeyboardMarkup(row_width=1)
-<<<<<<< Updated upstream
     welcome_button1 = types.KeyboardButton('change language')
     welcome_button2 = types.KeyboardButton('other option')
     markup.add(welcome_button1, welcome_button2)
     bot.send_message(message.chat.id, "you can:", reply_markup=markup)
-=======
-    welcome_button1 = types.KeyboardButton('Change language')
-    welcome_button2 = types.KeyboardButton('Other option')
-    markup.add(welcome_button1, welcome_button2)
-    bot.send_message(message.chat.id, "You can:", reply_markup=markup)
->>>>>>> Stashed changes
 
 
-@bot.message_handler(func=lambda message: message.text == 'Change language')
+@bot.message_handler(func=lambda message: message.text == 'change language')
 def message_handler(message):
     language_settings.open_languages_settings(message)
 
