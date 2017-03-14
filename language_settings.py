5350cdee7c5775a289579c58aaa1f384898447dc
tim-po <p.hna@mail.ru>
2017-03-14 22:52:58 +0300

    1.7.1
    function descriptions added

--- language_settings.py
+++ language_settings.py
@@ -6,9 +6,14 @@ import yandexAPI
 
 
 def change_language(message):
+    """
+
+    :param message: сообщение, которое отправил пользователь
+    :return: вносит в базу данных настройки языка пользователя
+    """
     database = {}
     bot = telebot.TeleBot(config.token)
-    database[message.from_user.id] = yandexAPI.get_supported_languages()[message.text]
+    database[message.from_user.id] = message.text[-3:-1]
     database_json = json.dumps(database)
     file = open('database.json', 'w')
     file.write(database_json + '\n')
@@ -17,14 +22,19 @@ def change_language(message):
 
 
 def open_languages_settings(message):
+    """
+
+    :param message: сообщение, которое отправил пользователь
+    :return: создает меню выбора языков
+    """
     markup_list = []
     itembtn_name = []
     yandexAPI_list = list(yandexAPI.get_supported_languages())
     bot = telebot.TeleBot(config.token)
     markup = types.ReplyKeyboardMarkup(row_width=1)
-    for i in range(90):
+    for i in range(len(yandexAPI_list)):
         itembtn_name.append('itmbtn' + str(i))
-        itembtn_namei = types.KeyboardButton(": " + yandexAPI.get_supported_languages()[yandexAPI_list[i]])
+        itembtn_namei = types.KeyboardButton(": " + yandexAPI.get_supported_languages()[yandexAPI_list[i]] + '(' + yandexAPI_list[i] + ")")
         markup_list.append(itembtn_name[i])
         markup.add(itembtn_namei)
         i += 1
@@ -33,6 +43,11 @@ def open_languages_settings(message):
 
 
 def get_user_language(user_id):
+    """
+
+    :param user_id: id юзера
+    :return: возвращает язык перевода конкретного юзера
+    """
     file = json.load(open('database.json', 'r'))
     return file[str(user_id)]
 
